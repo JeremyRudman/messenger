ca8f71c7adf956978cfc69d990ab353395d67515
jeremyrudman <jeremyrudman@gmail.com>
2019-07-13 13:22:52 -0400

    making homepage and sign-in for messaging app

---
+++ src/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './Header.css'
+
+
+export class Header extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = { username: "",
+        loggedin:false};
+
+    }
+
+
+
+    render() {
+        return(
+            <div id="Header">
+                <Link to={'/'}>
+                    <li>Home</li>
+                </Link>
+                {this.state.loggedIn ? (
+                    //TODO
+                    <div>implment logout</div>
+
+                ) : (
+                    <Link to={'/signin'}>
+                        <li>Sign In</li>
+                    </Link>
+                )}
+
+                <div className="user">
+                {this.state.username}
+                </div>
+            </div>
+
+        )
+
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,114 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import {Header} from './Header';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            loggedIn: false,
+        }
+    }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    setName(name) {
+        if (this.state.loggedIn === false && name !== "") {
+            this.setState({username: name});
+            this.setState({loggedIn: true});
+        }
+    }
+
+    render() {
+        const Home = () => (
+            <div>
+                <h1>Home Page {this.state.username}</h1>
+            </div>
+        );
+        return (
+            <div>
+                <Router>
+                    <Header username={this.state.username} loggedIn={this.state.loggedIn}
+                            username={this.state.username}/>
+                    <Switch>
+                        <Route path="/" exact component={Home}/>
+                        <Route path="/signin" render={(props) => <SignIn {...props} loggedin={this.state.loggedIn}
+                                                                         username={this.state.username}
+                                                                         setName={this.setName.bind(this)}/>}/>
+                        <Route path="/userhome" render={(props) => <SignedIn {...props} loggedin={this.state.loggedIn}
+                                                                             username={this.state.username}
+                                                                             setName={this.setName.bind(this)}/>}/>
+                    </Switch>
+                </Router>
+            </div>
+        )
+    }
+}
+
+class SignIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loggedIn: false,
+            username: "",
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({username: event.target.value});
+        this.props.setName(event.target.value);
+    }
+
+
+    handleSubmit(event) {
+        this.setState({loggedIn: true});
+        return (
+            <Redirect to="/"/>
+        )
+    }
+
+
+    render() {
+        // if (this.state.loggedIn === false) {
+            return (
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="text" value={this.state.location}
+                               onChange={this.handleChange}/>
+                        <input type="submit" value="Submit"/>
+
+                    </form>
+                </div>
+            )
+        // }
+
+    }
+}
+
+class SignedIn extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+        }
+    }
+
+    render() {
+        // if (this.state.username === "") {
+        //     return (
+        //         <Redirect to="/"/>
+        //     )
+        // }
+        return (
+            <div>
+                {this.props.username}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
